bcb25039907763e4ad1e02af6bdfd61d162c72b8
AKSHAY .C.S <akshaycsasidharan@gmail.com>
2024-09-20 06:00:54 -0700

    Backend successfully completed

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-import postRoutes from  "./routes/post.js";
+import postRoutes from "./routes/post.js";
+import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
- 
+import { verifyToken } from "./middleware/auth.js";
+
 // configurations
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,23 +40,20 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-
-
 // routes with files
-app.post("/auth/register",upload.single("picture"),register);
-
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // routes
-app.use("/auth",authRoutes);
-app.use("/users",userRoutes);
-app.use("/posts",postRoutes);
-
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // mongoose setup
 const PORT = process.env.PORT || 6001;
 
 mongoose
-  .connect(process.env.MONGO_URL)  
+  .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
