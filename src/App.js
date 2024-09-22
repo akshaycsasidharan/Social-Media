d35637c4ae9198182234315bb7e0ed3fae49b328
AKSHAY .C.S <akshaycsasidharan@gmail.com>
2024-09-22 11:37:06 -0700

    navbar successfully implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
